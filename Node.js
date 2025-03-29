7fd167155c34e80529c251f09259c6d2aedffd83
Yaroslav483 <yaroslav.niko2506@gmail.com>
2025-03-29 17:20:27 +0200

    Update Node.js

--- Node.js
+++ Node.js
@@ -1,1 +1,30 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+app.get('/hello', (req, res) => {
+    res.send('Hello world !');
+});
+
+
+app.get('/json', (req, res) => {
+    res.json({ message: 'Це JSON-відповідь' });
+});
+
+
+app.get('/user/:id', (req, res) => {
+    const userId = req.params.id;
+    res.send(`Користувач з ID: ${userId}`);
+});
+
+
+app.get('/search', (req, res) => {
+    const query = req.query.q;
+    res.send(`Пошук за запитом: ${query}`);
+});
+
+app.listen(port, () => {
+    console.log(`Сервер запущено на http://localhost:${port}`);
+});
 
