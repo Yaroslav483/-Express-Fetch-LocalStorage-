de0c5205a88332ca0188775a222f2817002f13a5
Yaroslav483 <yaroslav.niko2506@gmail.com>
2025-05-13 15:38:47 +0300

    Update index.js

--- Tapalka/index.js
+++ Tapalka/index.js
@@ -46,4 +46,28 @@ app.delete('/upgrades/:id', (req, res) => {
 app.listen(PORT, () => {
   console.log(`🚀 API працює на http://localhost:${PORT}`);
 });
+const express = require('express');
+const cors = require('cors');
+
+
+const user = require('./user');
+
+app.use(cors());
+app.use(express.json());
+
+app.post('/click', (req, res) => {
+  const result = user.click();
+  if (result.error) return res.status(result.code).json({ message: result.error });
+  return res.status(200).json(result);
+});
+
+app.post('/passive-income', (req, res) => {
+  const result = user.passiveIncome();
+  if (result.error) return res.status(result.code).json({ message: result.error });
+  return res.status(200).json(result);
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Сервер працює на http://localhost:${PORT}`);
+});
 
